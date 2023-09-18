99a529e338868c071da8426d3b5326acf5de6206
Leonardo Ruiz Bottura <92493176+LeonardoRuizB@users.noreply.github.com>
2023-09-18 13:50:38 -0300

    feat: Adjusting Footer css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,10 +3,14 @@ import styled from "styled-components";
 
 const MainFooter = styled.div`
     margin-top: 2%;
-    padding: 1px;
+    padding: 1%;
     text-align: center;
     background-color: white;
     font-size: 15px;
+
+    @media screen and (max-width: 480px) {
+        padding: 3%;
+      }
 `;
 
 const TextFooter = styled.p`
