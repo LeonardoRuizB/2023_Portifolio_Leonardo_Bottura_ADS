f6f6be36a12dad1b244cd9fff2fcbdf2fd2dc674
LeonardoRuizB <leobottura17@gmail.com>
2024-09-06 19:31:02 -0300

    feat: Creating Certifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContainerTop from './Components/Top/Container-top';
+import CarouselCertifications from './Components/CarouselCertifications/Carousel';
 import Carousel from './Components/Carousel/Carousel';
 import Footer from './Components/Footer/Footer';
 import Aboutme from './Components/About Me/About-me';
@@ -15,6 +16,7 @@ function App() {
       <Aboutme />
       <Education />
       <WorkExperience />
+      <CarouselCertifications />
       <Carousel />
       <SectionCards />
       <Footer />
---
+++ src/Components/CarouselCertifications/Carousel.jsx
@@ -0,0 +1,107 @@
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import "./Carousel.css";
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css';
+
+
+import { Pagination, Navigation } from 'swiper/modules';
+
+import slide_image_1 from "../../assets/certifications/Certificação Microsoft Azure AZ-900.png";
+import slide_image_2 from "../../assets/certifications/Certificação Microsoft Azure DP-900.png";
+
+import styled from 'styled-components';
+
+const TextCarousel = styled.h2`
+    margin-top: 5%;
+    text-align: center;
+    color: white;
+    font-size: 32px;
+
+    @media (max-width: 480px) {
+        margin-top: 12%;
+        margin-bottom: 10%;
+    }
+`;
+
+function CarouselCertifications() {
+    const [swiperRef, setSwiperRef] = useState(null);
+
+    let appendNumber = 4;
+    let prependNumber = 1;
+
+    const prepend2 = () => {
+        swiperRef.prependSlide([
+            '<div class="swiper-slide">Slide ' + --prependNumber + '</div>',
+            '<div class="swiper-slide">Slide ' + --prependNumber + '</div>',
+        ]);
+    };
+
+    const prepend = () => {
+        swiperRef.prependSlide(
+            '<div class="swiper-slide">Slide ' + --prependNumber + '</div>'
+        );
+    };
+
+    const append = () => {
+        swiperRef.appendSlide(
+            '<div class="swiper-slide">Slide ' + ++appendNumber + '</div>'
+        );
+    };
+
+    const append2 = () => {
+        swiperRef.appendSlide([
+            '<div class="swiper-slide">Slide ' + ++appendNumber + '</div>',
+            '<div class="swiper-slide">Slide ' + ++appendNumber + '</div>',
+        ]);
+    };
+
+    return (
+        <div className='container-carousel'>
+            <TextCarousel>Certificações</TextCarousel>
+            <Swiper
+                onSwiper={setSwiperRef}
+                slidesPerView={5}
+                centeredSlides={true}
+                spaceBetween={30}
+                pagination={{
+                    type: 'fraction',
+                }}
+                breakpoints={{
+                    1024: {
+                        slidesPerView: 5,
+                    },
+                    768: {
+                        slidesPerView: 2, 
+                    },
+                    480: {
+                        slidesPerView: 2, 
+                    },
+                    200: {
+                        slidesPerView: 1, 
+                    },
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide className='swiper-slide'>
+                    <a href={slide_image_1} target='_blank'>
+                        <img src={slide_image_1} alt="Certificação Microsoft AZ-900" />
+                        <p className="text-carousel">Certificação Microsoft AZ-900</p>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide className='swiper-slide'>
+                    <a href={slide_image_2} target='_blank'>
+                        <img src={slide_image_2} alt="Certificação Microsoft DP-900" />
+                        <p className="text-carousel">Certificação Microsoft DP-900</p>
+                    </a>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+}
+
+export default CarouselCertifications;
